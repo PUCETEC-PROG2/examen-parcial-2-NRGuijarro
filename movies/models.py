0e8ff5a9a195de22bce4689e4335f6dff5bbd034
Nelson <nrguijarro@puce.edu.ec>
2025-01-11 12:54:16 -0500

    Examen parcial 2

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Movie (models.Model):
+    id = models.AutoField(primary_key=True)
+    movie = models.CharField(max_length=30, null=False)
+    genre = {
+        ('A', 'Acción'),
+        ('T', 'Terror'),
+        ('D', 'Drama'),
+        ('C', 'Comedia'),
+    }
+    movie_genre = models.CharField(max_length=10, choices=genre, null=False)
+    name_director = models.CharField(max_length=30, null=False)
+    year= models.DateField(auto_now=False, null=False)
+    sinopsis = models.TextField(null=False)
+
+    def __str__(self):
+        return self.movie
